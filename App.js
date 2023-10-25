46a20b9526db5a8c97e5b55e68af7bd510efc875
William Lopera <lopera.william@gmail.com>
2023-10-24 19:34:45 -0500

    Inicio de proyecto enviar pushnotifications

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+// Definir como debe manejarse las notificaciones que recibe el dispositivo
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    console.log("Porcesar notificaciones")
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    };
+  },
+});
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    console.log("Enviar notificacion")
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "NOTIFICACION",
+        body: "Cuerpo de la notificación",
+        data: { ususario: "wlopera" },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Programar Notificacione"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
