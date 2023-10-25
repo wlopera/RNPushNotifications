46e55287df17a37e5fdfe2326e4a884a1e421c96
William Lopera <lopera.william@gmail.com>
2023-10-25 10:03:11 -0500

    Request y response de notificaciones

--- App.js
+++ App.js
@@ -1,22 +1,51 @@
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, View } from "react-native";
 import * as Notifications from "expo-notifications";
 
-// Definir como debe manejarse las notificaciones que recibe el dispositivo
+// Definir que debe manejarse las notificaciones que recibe el dispositivo
 Notifications.setNotificationHandler({
   handleNotification: async () => {
-    console.log("Porcesar notificaciones")
     return {
       shouldPlaySound: false,
       shouldSetBadge: false,
-      shouldShowAlert: true
+      shouldShowAlert: true,
     };
   },
 });
 
 export default function App() {
+  // Agregar detectores de notificaciones cuando refresque y que se elimene al cerrar el componente
+  // Controladores de eventos (eliminar)
+  useEffect(() => {
+    const suscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log(
+          "Notificación peticion recibida:",
+          JSON.stringify(notification, null, 2)
+        );
+        const username = notification.request.content.data.ususario;
+        console.log(username);
+      }
+    );
+
+    const suscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(
+          "Notificación respuesta recibida:",
+          JSON.stringify(response, null, 2)
+        );
+        const username = response.notification.request.content.data.ususario;
+        console.log(username);
+      }
+    );
+    return () => {
+      suscription1.remove();
+      suscription2.remove();
+    };
+  }, []);
+
   function scheduleNotificationHandler() {
-    console.log("Enviar notificacion")
     Notifications.scheduleNotificationAsync({
       content: {
         title: "NOTIFICACION",
